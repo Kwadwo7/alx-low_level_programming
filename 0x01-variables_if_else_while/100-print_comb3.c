a7d97f30d3a9620bcac7f26ff010aa4761fde119
Benedict Sarfo Appiah <appiahbenedict724@gmail.com>
2023-03-18 00:06:14 -0700

    updated 100 print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,7 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if (i+j != 17)
+				if (i + j != 17)
 				{
 					putchar(',');
 					putchar(' ');
@@ -28,3 +28,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
+}
