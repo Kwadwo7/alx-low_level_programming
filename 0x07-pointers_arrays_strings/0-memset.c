34f529cb9f8cb2f5fa9e74ddaa94947e6cc2c3c8
Benedict Sarfo Appiah <appiahbenedict724@gmail.com>
2023-04-03 14:44:31 +0000

    updated memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _memset - function for memset
+ * @s: pointer to the block of memory to fill
+ * @b: the value to fill the block of memory
+ * @n: the number of bytes to be set to the value
+ * Return: s for success
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	memset(s, b, n);
