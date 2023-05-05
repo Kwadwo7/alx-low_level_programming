5c70a1e6ad9211e0a3260fed839ba2f831bf1c04
Benedict Sarfo Appiah <appiahbenedict724@gmail.com>
2023-05-05 04:22:06 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the littleness or bigness of a machine
+ * endian
+ * Return: 0 for big and 1 for little
+ */
+
+int get_endianness(void)
+{
+	unsigned int k;
+	char *b = (char *) &k;
+
+	k = 1;
+	return (*c);
+}
